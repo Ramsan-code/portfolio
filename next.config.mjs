6f48d82c5650326a2f3d7217638993effa3d0edc
thaventhiran Ramsan <ramsandota@gmail.com>
2026-01-02 11:06:59 +0530

    chore: add ESLint config, framer-motion, and lucide-react while removing bundle analyzer and sharp.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
-  },
 };
 
-export default withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})(nextConfig);
-
+export default nextConfig;
